00325a511f765c53b0b5d543d2c21dfd3754eca8
Jacek Smolak <jacek.smolak@gmail.com>
2020-05-28 21:28:07 +0200

    Create files named after Schemas coming from schema data

--- test/e2e/code-generator/buildClasses.test.js
+++ test/e2e/code-generator/buildClasses.test.js
@@ -1,8 +1,29 @@
-import { describe, it } from 'mocha';
+import { describe, it, beforeEach, afterEach } from 'mocha';
 import { expect } from 'chai';
+import tmp from 'tmp';
+import path from 'path';
+import mkdirp from 'mkdirp';
+import rimraf from 'rimraf';
+import fs from 'fs';
 import { buildClasses } from '../../../src/code-generator/buildClasses';
+import schemaData from '../../dummy-data/schemaData.json';
 
 describe('buildClasses', () => {
+    const schemasInSchemaData = Object.keys(schemaData.schemas);
+    const testBuildFolder = 'build';
+    let tempDir;
+
+    beforeEach(() => {
+        tempDir = tmp.dirSync({ mode: 0o750, prefix: 'myTmpDir_' });
+        mkdirp.sync(path.resolve(tempDir.name, testBuildFolder));
+    });
+
+    afterEach(() => {
+        rimraf.sync(path.resolve(tempDir.name, testBuildFolder));
+
+        tempDir.removeCallback();
+    });
+
     describe('buildPath config option', () => {
         describe("when path doesn't exist", () => {
             it('should throw', () => {
@@ -11,5 +32,16 @@ describe('buildClasses', () => {
                 expect(() => buildClasses({ buildPath })).to.throw('Build path is not accessible.');
             });
         });
+    });
+
+    it('should create files named after Schemas coming from schema data', () => {
+        const buildPath = path.resolve(tempDir.name, testBuildFolder);
+
+        buildClasses({ buildPath, schemaData });
+
+        const files = fs.readdirSync(buildPath);
+        const schemaClassFileNames = schemasInSchemaData.map((schemaName) => `${schemaName}.js`);
+
+        expect(files).to.have.members(schemaClassFileNames);
     });
 });
