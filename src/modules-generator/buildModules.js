c21595c1d7953170e93779964a6098bcc6b6be5b
Jacek Smolak <jacek.smolak@gmail.com>
2020-06-07 23:04:20 +0200

    Have basic modules code created

---
+++ src/modules-generator/buildModules.js
@@ -0,0 +1,75 @@
+import fs from 'fs';
+import path from 'path';
+
+export const buildModules = ({ buildPath, schemaData }) => {
+    try {
+        fs.accessSync(buildPath);
+    } catch (_) {
+        throw new Error('Build path is not accessible.');
+    }
+
+    const schemaNames = Object.keys(schemaData.schemas);
+    const createFileName = (moduleName) => `${moduleName}.js`;
+    const isANumber = (character) => Number.isInteger(Number(character));
+    const ensureSchemaVariableNameIsSyntaxCompatible = (schemaName) => {
+        const firstCharacterIsANumber = isANumber(schemaName.charAt(0));
+
+        if (firstCharacterIsANumber) {
+            return `_${schemaName}`;
+        }
+
+        return schemaName;
+    };
+    const createOwnPropertiesCode = (properties) =>
+        properties.own
+            .reduce((code, propertyName) => {
+                return `${code}
+    ${propertyName}: () => {},`;
+            }, '')
+            .trim();
+    const createAncestorModulesImportCode = (parentSchemaNames) =>
+        parentSchemaNames
+            .reduce((code, moduleName) => {
+                const syntaxCompatibleSchemaName = ensureSchemaVariableNameIsSyntaxCompatible(moduleName);
+
+                return `${code}
+import ${syntaxCompatibleSchemaName} from './${moduleName}';`;
+            }, '')
+            .trim();
+    const createAncestorPropertiesInclusionCode = (parentSchemaNames) =>
+        parentSchemaNames
+            .reduce((code, moduleName) => {
+                const syntaxCompatibleSchemaName = ensureSchemaVariableNameIsSyntaxCompatible(moduleName);
+
+                return `${code}
+    ...${syntaxCompatibleSchemaName},`;
+            }, '')
+            .trim();
+
+    schemaNames.forEach(async (schemaName) => {
+        const fileName = createFileName(schemaName);
+        const filePath = path.resolve(buildPath, fileName);
+        const { properties } = schemaData.schemas[schemaName];
+
+        const parentSchemaNames = Object.keys(properties).filter(
+            (propertiesGroupName) => propertiesGroupName !== 'all' && propertiesGroupName !== 'own'
+        );
+
+        const ancestorModulesImportCode = createAncestorModulesImportCode(parentSchemaNames);
+        const schemaVariableName = ensureSchemaVariableNameIsSyntaxCompatible(schemaName);
+        const moduleBodyCode = `${
+            ancestorModulesImportCode ? createAncestorPropertiesInclusionCode(parentSchemaNames) : ''
+        }
+    ${createOwnPropertiesCode(properties)}`.trim();
+
+        const moduleCode = `${ancestorModulesImportCode}
+
+const ${schemaVariableName} = {
+    ${moduleBodyCode}
+}
+
+export default ${schemaVariableName};`.trim();
+
+        fs.writeFileSync(filePath, moduleCode);
+    });
+};
---
+++ test/e2e/modules-generator/buildModules.test.js
@@ -0,0 +1,127 @@
+import { describe, it, beforeEach, afterEach } from 'mocha';
+import { expect } from 'chai';
+import tmp from 'tmp';
+import path from 'path';
+import mkdirp from 'mkdirp';
+import rimraf from 'rimraf';
+import fs from 'fs';
+import { buildModules } from '../../../src/modules-generator/buildModules';
+import schemaData from '../../dummy-data/schemaData.json';
+
+describe('buildModules', () => {
+    const schemasInSchemaData = Object.keys(schemaData.schemas);
+    const testBuildFolder = 'build';
+    let tempDir;
+
+    beforeEach(() => {
+        tempDir = tmp.dirSync({ mode: 0o750, prefix: 'myTmpDir_' });
+        mkdirp.sync(path.resolve(tempDir.name, testBuildFolder));
+    });
+
+    afterEach(() => {
+        rimraf.sync(path.resolve(tempDir.name, testBuildFolder));
+
+        tempDir.removeCallback();
+    });
+
+    describe('buildPath config option', () => {
+        describe("when path doesn't exist", () => {
+            it('should throw', () => {
+                const buildPath = '/some/non-existent/or/non-accessible/build/path';
+
+                expect(() => buildModules({ buildPath })).to.throw('Build path is not accessible.');
+            });
+        });
+    });
+
+    it('should create modules named after Schemas coming from schema data', () => {
+        const buildPath = path.resolve(tempDir.name, testBuildFolder);
+
+        buildModules({ buildPath, schemaData });
+
+        const fileNames = fs.readdirSync(buildPath);
+        const expectedFileNames = schemasInSchemaData.map((schemaName) => `${schemaName}.js`);
+
+        expect(fileNames).to.have.members(expectedFileNames);
+    });
+
+    describe('created modules', () => {
+        it('should be objects', () => {
+            const buildPath = path.resolve(tempDir.name, testBuildFolder);
+
+            buildModules({ buildPath, schemaData });
+
+            const modules = schemasInSchemaData.map(async (schemaName) => {
+                const fileName = `${schemaName}.js`;
+                const filePath = path.resolve(buildPath, fileName);
+                const importedModule = await import(filePath);
+
+                return {
+                    module: importedModule.default,
+                    schemaName
+                };
+            });
+
+            return Promise.all(modules).then((resolvedModules) => {
+                resolvedModules.forEach(({ module }) => {
+                    expect(typeof module).to.equal('object');
+                });
+            });
+        });
+
+        it("should contain all schema's own properties as functions", () => {
+            const buildPath = path.resolve(tempDir.name, testBuildFolder);
+
+            buildModules({ buildPath, schemaData });
+
+            const modules = schemasInSchemaData.map(async (schemaName) => {
+                const fileName = `${schemaName}.js`;
+                const filePath = path.resolve(buildPath, fileName);
+                const importedModule = await import(filePath);
+
+                return {
+                    module: importedModule.default,
+                    schemaName
+                };
+            });
+
+            return Promise.all(modules).then((resolvedModules) => {
+                resolvedModules.forEach(({ module, schemaName }) => {
+                    const ownProperties = schemaData.schemas[schemaName].properties.own;
+
+                    ownProperties.forEach((propertyName) => {
+                        expect(typeof module[propertyName]).to.equal('function');
+                    });
+                });
+            });
+        });
+
+        it("should have access to properties inherited from all of schema's ancestors", () => {
+            const buildPath = path.resolve(tempDir.name, testBuildFolder);
+
+            buildModules({ buildPath, schemaData });
+
+            const modules = schemasInSchemaData.map(async (schemaName) => {
+                const fileName = `${schemaName}.js`;
+                const filePath = path.resolve(buildPath, fileName);
+                const importedModule = await import(filePath);
+
+                return {
+                    module: importedModule.default,
+                    schemaName
+                };
+            });
+
+            return Promise.all(modules).then((resolvedModules) => {
+                resolvedModules.forEach(({ module, schemaName }) => {
+                    const properties = schemaData.schemas[schemaName].properties;
+                    const ancestorProperties = properties.all.filter((property) => !properties.own.includes(property));
+
+                    ancestorProperties.forEach((propertyName) => {
+                        expect(typeof module[propertyName]).to.equal('function');
+                    });
+                });
+            });
+        });
+    });
+});
