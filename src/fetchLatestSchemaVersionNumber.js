2a7934b79d3536ecd52014f021445053721023e0
Jacek Smolak <jacek.smolak@gmail.com>
2020-07-04 23:57:53 +0200

    Return latest version number available today (not in the future)
    I just learned that there was a new version release logged, but ...
    it is due to be released. So I need to pick the latest available.

--- src/fetchLatestSchemaVersionNumber.js
+++ src/fetchLatestSchemaVersionNumber.js
@@ -5,12 +5,16 @@ const CONFIG_FILE_URL = 'https://raw.githubusercontent.com/schemaorg/schemaorg/m
 const versionDataFetcher = () =>
     fetch(CONFIG_FILE_URL)
         .then((res) => res.json())
-        .then((data) => {
-            return {
-                schemaVersionNumber: data.schemaversion
-            };
-        });
+        .then((data) => data);
 
 export const fetchLatestSchemaVersionNumber = (fetchVersionData = versionDataFetcher) => {
-    return fetchVersionData().then(({ schemaVersionNumber }) => schemaVersionNumber);
+    return fetchVersionData().then(({ releaseLog }) => {
+        const now = new Date();
+        const entries = Object.entries(releaseLog);
+        const [versionNumber] = entries.find(([, releaseDate]) => {
+            return now >= new Date(releaseDate);
+        });
+
+        return versionNumber;
+    });
 };
